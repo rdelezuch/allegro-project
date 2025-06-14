208e61cdeaae4b5338ce5cfc0347afd1c2909c32
rdelezuch <robert.delezuch@gmail.com>
2025-06-14 20:44:04 +0200

    Dodanie tailwindCSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui', '@nuxt/content'],
+  css: [
+      '~/assets/css/main.css',
+      '~/assets/css/style.css'
+  ],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
