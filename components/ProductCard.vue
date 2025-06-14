a231e5667751067f8ed4e2331bb8d402ec530e88
rdelezuch <robert.delezuch@gmail.com>
2025-06-14 22:19:43 +0200

    Dodanie sekcji z produktami i templatki z kartą pojedyńczego produktu

---
+++ components/ProductCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- Templatka pojedyńczego produktu z propsami -->
+  <div class="bg-[#2b2b2b] border border-[#3a3a3a] rounded-md p-2 text-white">
+    <img :src="image" alt="produkt" class="rounded mb-2 w-full h-40 object-cover" loading="lazy" />
+    <p class="text-sm font-semibold min-h-[48px]">{{ name }}</p>
+    <p class="text-green-400 font-bold text-lg mt-1">{{ price }} zł</p>
+    <p class="text-xs text-gray-400 mt-1">Dostawa: {{ delivery }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+<!-- Zdefiniowane propsy -->
+  defineProps<{
+    image: string
+    name: string
+    price: string | number
+    delivery: string
+  }>()
+</script>
---
+++ components/ProductSection.vue
@@ -0,0 +1,33 @@
+<template>Add commentMore actions
+  <section class="bg-[#222222] py-6 px-4">
+    <!-- Kontener z produktami -->
+    <div class="max-w-screen-xl mx-auto">
+      <h2 class="text-white text-xl font-semibold mb-4">{{ title }}</h2>
+      <!-- Wylistowane produkty  -->
+      <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+        <ProductCard
+            v-for="(product, index) in products"
+            :key="index"
+            :image="product.image"
+            :name="product.name"
+            :price="product.price"
+            :delivery="product.delivery"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import ProductCard from './ProductCard.vue'
+
+  defineProps<{
+    title: string
+    products: {
+      image: string
+      name: string
+      price: string | number
+      delivery: string
+    }[]
+  }>()
+</script>
